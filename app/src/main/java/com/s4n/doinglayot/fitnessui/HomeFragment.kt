e0cef347567c00fded0cc70d33d6953517bde589
julit <jtriana@copaair.com>
2019-04-04 02:53:33 -0500

    init

---
+++ app/src/main/java/com/s4n/doinglayot/fitnessui/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.s4n.doinglayot.fitnessui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.s4n.doinglayot.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cta.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_detailFragment)
+        }
+    }
+
+}
